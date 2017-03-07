657319f9503c668eab8c48e2cae9f4bf98185aad
anil.kumar <anil.kumar@RezD021.rezblr.local>
2017-03-07 12:09:06 +0530

    Initial checkin

---
+++ GitTest/src/GitTest/GitFirst.java
@@ -0,0 +1,17 @@
+package GitTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GitFirst {
+
+	public static void main(String args[])
+	{
+	WebDriver driver =new FirefoxDriver();
+	driver.get("http://www.google.com");
+	System.out.println("hi");
+	
+	
+	
+	}
+}
